0605d3878ce1aaa9a7e2aaafa6cf78baa3819b47
kym980518-debug <kym980518@gmail.com>
2026-04-02 21:01:40 +0900

    20260402

--- 06_scrapper/app.py
+++ 06_scrapper/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from main import search_incruit
+from file import save_to_file
 
 app = Flask(__name__)
 
@@ -18,6 +19,14 @@ def search():
 		keyword=keyword, 
 		jobs=enumerate(jobs)
 	)
+
+@app.route('/export')
+def export():
+	keyword = request.args.get("keyword")
+	jobs = search_incruit(keyword)
+	save_to_file(jobs)
+
+	return send_file("./jobs.csv", as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
